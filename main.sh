889802a27efb6cbc18dda8eaae97596d9da29005
Alexandra Marie Miller <accouts@alexandra.systems>
2019-08-04 22:52:21 -0700

    Initial commit

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+: '
+@project: tslides
+@repo: https://github.com/Alexandra-Miller/tslides
+@creator: Alexandra Marie Miller
+@description
+
+@description
+@dependencies: bash 
+'
+
+# ====================  CONSTANTS  =============================================
+
+HELPMSG="USAGE: tslides
+OPTIONS:
+
+"
+
+# ====================  FUNCTIONS  =============================================
